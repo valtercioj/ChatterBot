aa4d0b8f921efcee7f0959b58f2bca9744563ce1
valtercioj <53585971+valtercioj@users.noreply.github.com>
2019-10-15 17:14:11 -0300

    Update chatBot.py

--- chatBot.py
+++ chatBot.py
@@ -1,17 +1,17 @@
 from chatterbot import ChatBot
-bot = ChatBot("Ron Obvious")
+bot = ChatBot("Test")#,read_only=True)  <- read to stop training the bot
 from chatterbot.trainers import ListTrainer
 
-chats = ['oi', 'ola', 'tudo bom?', 'tudo e voce?', 'estou bem obrigado.']
+chats = ['oi', 'ola', 'tudo bom?', 'tudo e voce?', 'estou bem obrigado.'] # chats
 
-trainer = ListTrainer(bot)
+trainer = ListTrainer(bot) # training
 
-trainer.train(chats)
+trainer.train(chats) # training with the chats
 
 while True:
-    quest = input('Voce: ')
+    quest = input('You: ') 
 
-    response = bot.get_response(quest)
+    response = bot.get_response(quest) # bot response
 
-    print('Bot: ', response)
+    print('Bot: ', response) 
 
